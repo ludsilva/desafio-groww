253e37f40f4475326e00ecbccae636fc793a7573
Lu Silva <lusilva.91@protonmail.com>
2021-07-31 15:45:46 -0300

    Modificações no main.js

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-//Consumindo os dados de uma API
-/*
-fetch('https://safe-sierra-45694.herokuapp.com/user')
-    .then(T => T.json())
-    .then(console.log)
-    .then()
-*/
-    //Funcionou!!! Retornou um objeto em json
+//Chamar os dados da API e armazenar em uma variável
+
+let data = '';
+async function readApi(){
+    let url = 'https://safe-sierra-45694.herokuapp.com/user';
+    try{
+        let response = await fetch(url);
+        data = await response.json();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+readApi();
+
+async function main(){
+    await readApi();
+    retornarNomeIdade(data);
+}
+main();
 
 //Criar uma função que retorne o nome do usuário e sua idade
+/*
 const url = 'https://safe-sierra-45694.herokuapp.com/user';
+
 function retornarNomeIdade(url){
     console.log(`O nome do usuário é ${url["nome"]}`);
 }
 
-retornarNomeIdade();
+// retornarNomeIdade();
+*/
+
+function retornarNomeIdade(data){
+    console.log(`Usuário: ${data.usuario} e idade: ${data.idade}`);
+}
