f24e1bf1bf97b800528fe0fd638594ceb24d79d5
Nathália Barros <nathynanda@gmail.com>
2021-07-30 22:33:14 -0300

    update.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 //Consumindo os dados de uma API
+/*
 fetch('https://safe-sierra-45694.herokuapp.com/user')
     .then(T => T.json())
     .then(console.log)
-//Funcionou!!! Retornou um objeto em json
+    .then()
+*/
+    //Funcionou!!! Retornou um objeto em json
 
 //Criar uma função que retorne o nome do usuário e sua idade
+const url = 'https://safe-sierra-45694.herokuapp.com/user';
+function retornarNomeIdade(url){
+    console.log(`O nome do usuário é ${url["nome"]}`);
+}
 
+retornarNomeIdade();
