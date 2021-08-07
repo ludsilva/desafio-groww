ef7827b493ef7502b5254856caa41eec4c42da42
Lu Silva <lusilva.91@protonmail.com>
2021-08-07 19:56:06 -0300

    Resolução dos exercícios

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 //Chamar os dados da API e armazenar em uma variável
 
 let data = '';
@@ -15,21 +16,75 @@ readApi();
 
 async function main(){
     await readApi();
+    testeData(data);
+
     retornarNomeIdade(data);
+
+    typeOfDocumentAndNumber(data);
+    enderecoCompleto(data);
+    taxaEDataEntrega(data);
+    nomeValoreQtde(data);
+    qtdeProdutosVendidos(data);
+    processaCompra(data);
+    statusELinkPagamento(data);
 }
 main();
 
+const testeData = (data) => {console.log(data)};
+
 //Criar uma função que retorne o nome do usuário e sua idade
-/*
-const url = 'https://safe-sierra-45694.herokuapp.com/user';
-
-function retornarNomeIdade(url){
-    console.log(`O nome do usuário é ${url["nome"]}`);
-}
-
-// retornarNomeIdade();
-*/
 
 function retornarNomeIdade(data){
     console.log(`Usuário: ${data.usuario} e idade: ${data.idade}`);
 }
+
+//Criar uma função que retorna o tipo de documento e o número dele.
+function typeOfDocumentAndNumber(data){
+    console.log(`Tipo do documento: ${data.documentType}. Número do documento: ${data.documentNumber}.`)
+}
+
+//Criar uma função que retorna o endereço completo para entrega do produto
+const enderecoCompleto = (data) => {
+    const { 
+        transacao: {
+          shipping: {
+            address: { 
+              street, street_number, city, state, country, zipcode 
+            }, 
+          }, 
+        }, 
+    } = data; 
+    let message = `Endereço de entrega: \n${street}, n ${street_number}, cidade: ${city}, estado: ${state}. \nCEP: ${zipcode}. País: ${country}`
+    console.log(message);
+}; //P.S.: aqui eu tentei desestruturar o json, porque achei que ia ficar "deselegante" usar a notação de ponto em todas as propriedades
+
+// Criar uma função que retorna o valor da taxa de entrega e a data de entrega
+const taxaEDataEntrega = (data) => {
+    const {transacao: {shipping: {fee, delivery_date }, }, } = data;
+    let message = `Valor da entrega: R$ ${fee}. \nData da entrega: ${delivery_date}`;
+    console.log(message);
+};
+
+//  Criar uma função que retorna o nome do produto, o valor e a quantidade vendida
+const nomeValoreQtde = (data) => {
+    const {transacao: {items: {[0]: {title, quantity}, }, }, } = data; 
+    let message = `Produto: ${title}. \nQuantidade: ${quantity}.`;
+    console.log(message);
+};
+
+//  Criar uma função que retorna a quantidade de produtos sendo vendidas
+function qtdeProdutosVendidos(data){
+    return console.log(`Quantidade vendida: ${data.transacao.items[0].quantity}`);
+}
+// Criar uma função que processa uma compra (Quando chamar essa função, diminuir o total de itens para venda)
+function processaCompra(data){
+    const {links_pagamento: {items: {[0]: {quantity}, }, }, } = data;
+    if (data.transacao.status == "autorizado"){
+        data.links_pagamento.items[0].quantity = quantity - data.transacao.items[0].quantity;
+    }
+    return console.log(`Quantidade de itens: ${data.links_pagamento.items[0].quantity}`);
+}
+//  Criar uma função que retorna o status e o link para o pagamento
+function statusELinkPagamento(data){
+    return console.log(`Status: ${data.links_pagamento.status} \nLink de pagamento: ${data.links_pagamento.url}`);
+}   
